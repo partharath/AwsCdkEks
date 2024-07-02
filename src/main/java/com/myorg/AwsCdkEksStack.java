28cbe24165a4eae52e6a234d2129e68e27a7994f
HP\user <partha.rath@gmail.com>
2024-07-02 12:13:30 +0530

    Update EKS stack

--- src/main/java/com/myorg/AwsCdkEksStack.java
+++ src/main/java/com/myorg/AwsCdkEksStack.java
@@ -1,10 +1,13 @@
 package com.myorg;
 
+import software.amazon.awscdk.services.ec2.*;
+import software.amazon.awscdk.services.eks.Cluster;
+import software.amazon.awscdk.services.eks.KubernetesVersion;
+import software.amazon.awscdk.services.rds.SubnetGroup;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-// import software.amazon.awscdk.Duration;
-// import software.amazon.awscdk.services.sqs.Queue;
+
 
 public class AwsCdkEksStack extends Stack {
     public AwsCdkEksStack(final Construct scope, final String id) {
@@ -14,11 +17,29 @@ public class AwsCdkEksStack extends Stack {
     public AwsCdkEksStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
-        // The code that defines your stack goes here
 
-        // example resource
-        // final Queue queue = Queue.Builder.create(this, "AwsCdkEksQueue")
-        //         .visibilityTimeout(Duration.seconds(300))
-        //         .build();
+        // Create a VPC for the EKS cluster
+        Vpc vpc = Vpc.Builder.create(this, "VPC")
+                .maxAzs(3)
+                .build();
+
+        SubnetGroup publicSubnetGroup = SubnetGroup.Builder.create(this, "MyPublicSubnetGroup")
+                .vpc(vpc)
+                .description("Subnet group for public subnets")
+                .vpcSubnets(SubnetSelection.builder()
+                        .subnetType(SubnetType.PUBLIC)
+                        .build())
+                .build();
+
+
+
+        // Create the EKS cluster
+        Cluster cluster = Cluster.Builder.create(this, "EKSCluster")
+                .clusterName("my-eks-cluster")
+                .version(KubernetesVersion.V1_30)
+                .vpc(vpc)
+                .defaultCapacity(2)
+                .defaultCapacityInstance(InstanceType.of(InstanceClass.T2,InstanceSize.MICRO))
+                .build();
     }
 }
